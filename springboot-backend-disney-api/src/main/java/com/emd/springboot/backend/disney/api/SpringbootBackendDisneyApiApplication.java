876a003c7194b737d511d80139ff768bb2121171
Edgardo Mora <edgardomora@hotmail.com>
2021-12-15 15:19:24 -0300

    commit inicial

---
+++ springboot-backend-disney-api/src/main/java/com/emd/springboot/backend/disney/api/SpringbootBackendDisneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.emd.springboot.backend.disney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendDisneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendDisneyApiApplication.class, args);
+	}
+
+}
---
+++ springboot-backend-disney-api/src/test/java/com/emd/springboot/backend/disney/api/SpringbootBackendDisneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emd.springboot.backend.disney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendDisneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
